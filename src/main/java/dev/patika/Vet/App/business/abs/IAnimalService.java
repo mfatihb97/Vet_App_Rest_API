6068e17b7a61078ec12eb9c20271409a29ad067c
fatih batur <mfatihbatur@gmail.com>
2023-12-06 11:02:37 +0300

    Vet App

---
+++ src/main/java/dev/patika/Vet/App/business/abs/IAnimalService.java
@@ -0,0 +1,13 @@
+package dev.patika.Vet.App.business.abs;
+
+import dev.patika.Vet.App.entity.Animal;
+
+import java.util.List;
+
+public interface IAnimalService {
+    Animal getByID(int id);
+    Animal save(Animal animal);
+    String delete(int id);
+    Animal update(Animal animal);
+    List<Animal> findAll();
+}
