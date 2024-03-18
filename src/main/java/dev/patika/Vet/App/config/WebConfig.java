9c77fdebc198d0622bc0c5c130f1a1a21d330750
fatih batur <mfatihbatur@gmail.com>
2024-03-18 13:26:54 +0300

    Vet App Update

--- src/main/java/dev/patika/Vet/App/config/WebConfig.java
+++ src/main/java/dev/patika/Vet/App/config/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("https://vet-app-pmc9.onrender.com")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true)
                 .allowedHeaders("*")
